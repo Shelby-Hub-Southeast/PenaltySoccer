006e6a6eb7152af6b8d2554f7b7e3aa1ddab9bfc
Shelby-Hub-Southeast <106645317+Shelby-Hub-Southeast@users.noreply.github.com>
2026-06-06 01:25:19 +0800

    Add ratings package placeholder

---
+++ penaltysoccer/ratings/__init__.py
@@ -0,0 +1,5 @@
+"""Application-level rating features.
+
+This package will later integrate penaltyblog's Elo, Massey, Colley, and Pi
+rating systems into prediction reports and ensemble calibration.
+"""
