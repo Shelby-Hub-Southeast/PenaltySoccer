b9da1d03e7327924edfefd222f32d4117e2c8480
Shelby-Hub-Southeast <106645317+Shelby-Hub-Southeast@users.noreply.github.com>
2026-06-06 19:25:27 +0800

    Export Markdown reporting helpers

--- penaltysoccer/reporting/__init__.py
+++ penaltysoccer/reporting/__init__.py
@@ -1,11 +1,14 @@
 """Report rendering and serialization utilities."""
 
 from .json_report import dataclass_to_dict, save_reports_json
+from .markdown_report import render_reports_markdown, save_reports_markdown
 from .terminal_report import print_prediction_report, print_reports_summary
 
 __all__ = [
     "dataclass_to_dict",
     "print_prediction_report",
     "print_reports_summary",
+    "render_reports_markdown",
     "save_reports_json",
+    "save_reports_markdown",
 ]
