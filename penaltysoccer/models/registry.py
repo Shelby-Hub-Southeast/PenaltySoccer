a71550b099cf8a376eca20314b778e2eba6f0f04
Shelby-Hub-Southeast <106645317+Shelby-Hub-Southeast@users.noreply.github.com>
2026-06-06 14:13:01 +0800

    Register all available goal models

--- penaltysoccer/models/registry.py
+++ penaltysoccer/models/registry.py
@@ -13,6 +13,8 @@ MODEL_REGISTRY = {
     "negative_binomial": pb.models.NegativeBinomialGoalModel,
     "zero_inflated": pb.models.ZeroInflatedPoissonGoalsModel,
     "weibull_copula": pb.models.WeibullCopulaGoalsModel,
+    "bayesian": pb.models.BayesianGoalModel,
+    "hierarchical_bayesian": pb.models.HierarchicalBayesianGoalModel,
 }
 
 MODEL_DISPLAY_NAMES = {
@@ -22,4 +24,6 @@ MODEL_DISPLAY_NAMES = {
     "negative_binomial": "Negative Binomial",
     "zero_inflated": "Zero-Inflated Poisson",
     "weibull_copula": "Weibull Copula",
+    "bayesian": "Bayesian Goal Model",
+    "hierarchical_bayesian": "Hierarchical Bayesian Goal Model",
 }
